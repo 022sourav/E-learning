e5da7a9f8a70184095566acef1788da5b9f45c49
022sourav <90570643+022sourav@users.noreply.github.com>
2023-07-24 23:39:11 +0530

    Add files via upload

---
+++ E-Learning/index.js
@@ -0,0 +1,7 @@
+var computer = document.getElementById('comp');
+var it = document.getElementById('it');
+var entc = document.getElementById('entc');
+
+computer.addEventListener('click',()=>{
+    
+})
---
+++ E-Learning/tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        String name="Sourabh";
+        //variable
+        int age =20;
+        double price =50;
+        int a =25;
+        int b =10;
+        
+    }
+}
